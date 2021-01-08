541b50b46e34d290a19846c7785e6d6e68faa863
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-09 04:15:33 +0900

    update

---
+++ BOJ_PSC/11232/cmake-build-debug/CMakeFiles/11232.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "11232"
+  "11232.pdb"
+  "CMakeFiles/11232.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11232.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
