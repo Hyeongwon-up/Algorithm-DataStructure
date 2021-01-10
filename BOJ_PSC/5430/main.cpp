8b1b31f03fa7d463c19ebd630080ff76fd7229c6
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-10 17:45:09 +0900

    210109

--- BOJ_PSC/5430/main.cpp
+++ BOJ_PSC/5430/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <stack>
+#include <list>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    int T;
+    string cmd;
+
+    stack<int> st;
+
+
+    cin >> T;
+
+    while(T--) {
+
+
+        cin >> cmd;
+        int i = 0;
+        int arr[100];
+        int length = sizeof(cmd)/sizeof(string);
+
+        for(string::iterator iter = cmd.begin(); iter != cmd.end(); ++iter) {
+            arr[i] = *iter;
+        }
+
+        for(int i = 0; )
+
+
+
+
+
+
+    }
+
+
+
 }
